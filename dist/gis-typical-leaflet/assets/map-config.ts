c3f15bc228bdd20d77dfc771f909cd8c0d65b345
Deepak Devanand <deepakd.coding@gmail.com>
2022-12-11 21:36:43 +0530

    gh-pages

---
+++ dist/gis-typical-leaflet/assets/map-config.ts
@@ -0,0 +1,8 @@
+export const mapConfig = {
+    "centers": {
+        "bangalore": [ 12.9716,77.5946 ]
+    },
+    "tilelayer": {
+        "streetmap": "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    }
+}
