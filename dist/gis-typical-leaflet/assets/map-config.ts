ee6accbc8f0c7d94b0c3d34ceba4ad5e0f156d73
Deepak Devanand <deepakd.coding@gmail.com>
2022-12-12 19:13:07 +0530

    Auto-generated commit [ci skip]

--- dist/gis-typical-leaflet/assets/map-config.ts
+++
@@ -1,8 +0,0 @@
-export const mapConfig = {
-    "centers": {
-        "bangalore": [ 12.9716,77.5946 ]
-    },
-    "tilelayer": {
-        "streetmap": "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    }
-}
